78009b303e89c1efc5cbf81b282d0fe31577d701
unknown <spnaveen675@gmail.com>
2022-04-16 20:27:48 +0530

    installed node packages

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-
 express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+console.log(__dirname)
 app.get('/', (req,res) => {
     
     res.send("hello world")
