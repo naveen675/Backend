539c241f917c5044fd523bd6c64a8e906365cd19
unknown <spnaveen675@gmail.com>
2022-04-16 16:50:12 +0530

    Added Post method

---
+++ app.js
@@ -0,0 +1,13 @@
+
+express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    
+    res.send("hello world")
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at port: ${port}`)
+  });
